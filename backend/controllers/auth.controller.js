876774b764268350c34feae48d09aa14816edad2
Prathviesh1211 <prathvieshnaik@gmail.com>
2025-03-13 11:52:35 +0530

    Add Login and Logout route endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,8 +78,39 @@ export const verifyEmail = async (req, res) => {
 };
 export const login=async (req,res)=>{
 
+    const {email,password}=req.body;
+    try{
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({success:false,message:"Invlaid credentials"})
+        }
+
+        const isPasswordValid=await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({success:false,message:"Invlaid credentials"})
+        }
+
+        generateTokenAndSetCookie(res,user._id);
+
+        user.lastLogin=new Date();
+        await user.save();
+
+        res.status(200).json({
+            success:true,
+            message:"Logged in successfully",
+            user:{
+                ...user._doc,
+                password:undefined,
+            },
+        });
+
+    }catch(error){
+        console.error("error in login",error)
+        res.status(400).json({success:false,message:error.message})
+    }
 }
 
 export const logout=async (req,res)=>{
-
+    res.clearCookie("token");
+    res.status(200).json({success:true,message:"Logged out Successfully"})
 }
