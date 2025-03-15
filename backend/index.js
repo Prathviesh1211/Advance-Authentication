53566f4b803e71a60b8d1c32cc9426015e763b09
Prathviesh1211 <prathvieshnaik@gmail.com>
2025-03-15 21:41:03 +0530

    addcheckAuth endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express"
-import { connectDb } from "./db/connectDB.js";
-import authRoutes from './routes/auth.route.js'
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from './routes/auth.route.js'
+import { connectDb } from "./db/connectDB.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app=express();
 const PORT=process.env.PORT;
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/',(req,res)=>{
     res.send("Hello world")
